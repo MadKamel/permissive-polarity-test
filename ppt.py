390ee5819435928774a18e72a88bac1e0c10039b
Eric Rustrum <rustrumek@gmail.com>
2020-01-10 10:56:42 -0800

    Added comments to the top of the file
    -_-

--- ppt.py
+++ ppt.py
@@ -1,15 +1,34 @@
+#
+#-------------------------------------------------------------------------------------------------------------------------------------
+#         PPT (Permissive Polarity Test)
+#                 by Eric Rustrum
+#
+#             This program/module takes user input as a yes or no answer and translates it into a boolean value or polarity
+#                 value, such as +1 or -3, with '+' being yes and '-' being no. If the user gives an amibiguous answer, then
+#                 the module will automatically return a default value of False. To change the default return, the 'default'
+#                 argument in the main method, 'requestPermissionFromUser' needs to be either True or False, depending on the
+#                 desired default return value.
+#
+#--------------------------------------------------------------------------------------------------------------------------------------
+#
+# NOTE: because of the nature of the expected inputs, the most useful application for this would be in voice commands for yes/no
+#            questions.
+#
+#
+
+
 positive_keywords = ['may', 'nice', 'please', 'yeet', 'want', 'ok', 'sure', 'can', 'good', 'yes', 'positive', 'true', 'yeah', 'affirmative', 'fine']
 negative_keywords = ['dont', 'cant', 'bad', 'no', 'negative', 'false', 'stop', 'not']
 neutral_keywords = ['maybe', 'neutral', 'um', 'guess']
 strong_negative_keywords = ['never']
 strong_positive_keywords = ['must']
-not_neut_keys = ['sure']
-must_neg_keys = ['never', 'not', 'stop']
-must_pos_keys = ['do']
-go_pos_keys = ['ahead']
-do_pos_keys = ['it']
-do_neg_keys = ['not']
-i_pos_keys = ['guess', 'suppose', 'approve']
+not_neut_keys = ['sure'] # as in 'not sure'
+must_neg_keys = ['not'] # as in 'must not'
+must_pos_keys = ['do'] # as in 'must do'
+go_pos_keys = ['ahead'] # as in 'go ahead'
+do_pos_keys = ['it'] # as in 'do it'
+do_neg_keys = ['not'] # as in 'do not'
+i_pos_keys = ['guess', 'suppose', 'approve'] # as in 'I suppose' or 'I approve'
 
 def shiftToAbs(value, pos=1, neg=1):
   if value == 0:
